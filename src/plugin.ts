dfdc798339d4935b76ac3897d08194bb6e68b9e1
PontesEdu <edupontessilva03@gmail.com>
2025-08-08 12:00:00 -0300

    feat: add preset save and recall actions

--- src/plugin.ts
+++ src/plugin.ts
@@ -7,6 +7,9 @@ import { PTZTracking } from "./actions/ptz-tracking";
 import { PTZRegister } from "./actions/ptz-register";
 import { PtzExposure } from "./actions/ptz-exposure";
 import { PTZSpeed } from "./actions/pan-speed";
+import { PTZRecord } from "./actions/ptz-record";
+import { PTZPreset } from "./actions/preset";
+import { PTZSet } from "./actions/set-action";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
@@ -19,6 +22,9 @@ streamDeck.actions.registerAction(new PTZTracking());
 streamDeck.actions.registerAction(new PTZRegister());
 streamDeck.actions.registerAction(new PtzExposure());
 streamDeck.actions.registerAction(new PTZSpeed());
+streamDeck.actions.registerAction(new PTZRecord());
+streamDeck.actions.registerAction(new PTZPreset());
+streamDeck.actions.registerAction(new PTZSet());
 
 streamDeck.connect();
 
